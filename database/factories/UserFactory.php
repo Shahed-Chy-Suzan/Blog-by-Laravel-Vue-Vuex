31f32f361e58d74fceacf3b99b37ff92a2a055ea
shahed-chy-suzan <shahedchysuzan@gmail.com>
2020-10-12 02:11:05 +0600

    Generated dummy data in DB

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -23,3 +23,21 @@ $factory->define(App\User::class, function (Faker $faker) {
         'remember_token' => Str::random(10),
     ];
 });
+
+//----Newly added by us---------
+$factory->define(App\Category::class, function (Faker $faker) {
+  return [
+      'cat_name' => $faker->name,
+  ];
+});
+
+$factory->define(App\Post::class, function (Faker $faker) {
+  return [
+      'cat_id' =>  rand(1,10),
+      'user_id' =>  rand(1,10),
+      'comment_id' =>  rand(1,10),
+      'title' =>  $faker->sentence,
+      'description' =>  $faker->paragraph,
+      'photo' =>  $faker->imageUrl,
+  ];
+});
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -12,5 +12,9 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         // $this->call(UsersTableSeeder::class);
+        
+        factory(App\Post::class,10)->create();
+        factory(App\Category::class,10)->create();
+        factory(App\User::class,10)->create();
     }
 }
